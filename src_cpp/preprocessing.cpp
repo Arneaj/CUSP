e475d7326aa7e29243dc2ccbf5123ccec330a9f7
avr24 <avr24@ic.ac.uk>
2025-07-25 15:37:05 +0100

    right so I believe that in many cases the interest points have become so certain with the fixed preprocessing that the fitting cost is higher because the function is just not good enough to fit properly... I might need to get back to the drawing board regarding analytical functions aaaaaa. might try to limit the range of theta to something like [0;2*pi/3] to limit the negative impact the far night side could have on the fit. also removed the debugging in preprocessing.

--- src_cpp/preprocessing.cpp
+++ src_cpp/preprocessing.cpp
@@ -33,13 +33,13 @@ Matrix orthonormalise( const Matrix& mat, Matrix& X, Matrix& Y, Matrix& Z, const
         iX[sx] = i;
         dX[sx] = ( sx - X[i]) / (X[i+1] - X[i]);
 
-        if ( dX[sx] > 10 )
-        {
-            std::cout << "sx = " << sx << std::endl;
-            std::cout << "X[i] = " << X[i] << std::endl;
-            std::cout << "X[i+1] = " << X[i+1] << std::endl;
-            std::cout << "dX[sx] = " << dX[sx] << std::endl;
-        }
+        // if ( dX[sx] > 10 )
+        // {
+        //     std::cout << "sx = " << sx << std::endl;
+        //     std::cout << "X[i] = " << X[i] << std::endl;
+        //     std::cout << "X[i+1] = " << X[i+1] << std::endl;
+        //     std::cout << "dX[sx] = " << dX[sx] << std::endl;
+        // }
 
         break;
     }
@@ -71,16 +71,16 @@ Matrix orthonormalise( const Matrix& mat, Matrix& X, Matrix& Y, Matrix& Z, const
                                 +   ( mat(iX[sx],iY[sy],iZ[sz]+1,i)*(1-dX[sx]) + mat(iX[sx]+1,iY[sy],iZ[sz]+1,i)*dX[sx] )*(1-dY[sy])*dZ[sz]
                                 +   ( mat(iX[sx],iY[sy]+1,iZ[sz]+1,i)*(1-dX[sx]) + mat(iX[sx]+1,iY[sy]+1,iZ[sz]+1,i)*dX[sx] )*dY[sy]*dZ[sz];   
                                 
-            if ( new_mat(sx,sy,sz,i) > 1 )
-            {
-                std::cout << "sx = " << sx << "; sy = " << sy << "; sz = " << sz << "; i = " << i << std::endl;
-                std::cout << "iX[sx] = " << iX[sx] << "; iY[sy] = " << iY[sy] << "; iZ[sz] = " << iZ[sz] << std::endl;
-                std::cout << "shape.x = " << shape.x << std::endl;
-                std::cout << "dX[iX[sx]] = " << dX[iX[sx]] << "; dY[iY[sy]] = " << dY[iY[sy]] << "; dZ[iZ[sz]] = " << dZ[iZ[sz]] << std::endl;
-                std::cout << "mat(iX[sx],iY[sy],iZ[sz],i) = " << mat(iX[sx],iY[sy],iZ[sz],i) << std::endl;
-                std::cout << "mat(iX[sx]+1,iY[sy]+1,iZ[sz]+1,i) = " << mat(iX[sx]+1,iY[sy]+1,iZ[sz]+1,i) << std::endl;
-                std::cout << "new_mat(sx,sy,sz,i) = " << new_mat(sx,sy,sz,i) << std::endl;
-            }
+            // if ( new_mat(sx,sy,sz,i) > 1 )
+            // {
+            //     std::cout << "sx = " << sx << "; sy = " << sy << "; sz = " << sz << "; i = " << i << std::endl;
+            //     std::cout << "iX[sx] = " << iX[sx] << "; iY[sy] = " << iY[sy] << "; iZ[sz] = " << iZ[sz] << std::endl;
+            //     std::cout << "shape.x = " << shape.x << std::endl;
+            //     std::cout << "dX[iX[sx]] = " << dX[iX[sx]] << "; dY[iY[sy]] = " << dY[iY[sy]] << "; dZ[iZ[sz]] = " << dZ[iZ[sz]] << std::endl;
+            //     std::cout << "mat(iX[sx],iY[sy],iZ[sz],i) = " << mat(iX[sx],iY[sy],iZ[sz],i) << std::endl;
+            //     std::cout << "mat(iX[sx]+1,iY[sy]+1,iZ[sz]+1,i) = " << mat(iX[sx]+1,iY[sy]+1,iZ[sz]+1,i) << std::endl;
+            //     std::cout << "new_mat(sx,sy,sz,i) = " << new_mat(sx,sy,sz,i) << std::endl;
+            // }
         }
     
     return new_mat;
