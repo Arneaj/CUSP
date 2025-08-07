c813234e2209c1b8cadbe167a9c44c40a913f3d8
avr24 <avr24@ic.ac.uk>
2025-08-07 15:16:55 +0100

    made it so that the library test can take a filepath as a command line argument

--- python/library_test.py
+++ python/library_test.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 import topology_analysis as ta
 import gorgon
 
-Rho = gorgon.import_from_bin( "../data/Run1_23100/Rho_processed_real.bin" )
+import sys
+
+if len(sys.argv) < 2:
+    print("No Run path given!")
+    exit(1)
+
+filepath = sys.argv[1]
+
+Rho = gorgon.import_from_bin( filepath + "/Rho_processed_real.bin" )
 P_E = ta.Point(30, 58, 58)
 earth_pos = [30, 58, 58]
 
@@ -13,7 +21,7 @@ print("Bowshock radius for (theta,phi) = (0.0, 0.0):", bs_radius)
 BS = ta.get_bowshock( Rho, P_E, 0.1, 50, 50 )
 # print("All bowshock radii:", BS)
 
-J_norm = gorgon.import_from_bin( "../data/Run1_23100/J_norm_processed_real.bin" )
+J_norm = gorgon.import_from_bin( filepath + "/J_norm_processed_real.bin" )
 
 X, Y, Z = gorgon.spherical_to_cartesian( BS[:,2], BS[:,0], BS[:,1], earth_pos )
 
@@ -22,6 +30,6 @@ is_in_plane = np.abs(Y-58) < 2
 X_plot = X[is_in_plane]
 Z_plot = Z[is_in_plane]
 
-plt.imshow( J_norm[:,58,:], cmap="inferno", norm="log" )
+plt.imshow( Rho[:,58,:], cmap="inferno", norm="log" )
 plt.scatter( Z_plot, X_plot, s=1.0 )
 plt.savefig( "../images/bowshock.svg" )
