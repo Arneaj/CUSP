0ffeab479ec8be9d2749f4526d647769d798f9a8
avr24 <avr24@ic.ac.uk>
2025-08-19 21:29:01 +0100

    modified the run.sh job to import the proper modules

--- jobs/run.sh
+++ jobs/run.sh
@@ -6,21 +6,11 @@
 cd $PBS_O_WORKDIR
 
 exec > ../.result_folder/${PBS_JOBNAME}.o${PBS_JOBID%%.*} 2>&1
-rm ${PBS_JOBNAME}.o${PBS_JOBID%%.*} 2>&1
+# rm ${PBS_JOBNAME}.o${PBS_JOBID%%.*} 2>&1
 
 # echo $OMP_NUM_THREADS
 
-module load miniforge/3 > /dev/null 2>&1
-module load tools/prod > /dev/null 2>&1
-module load SciPy-bundle/2023.07-gfbf-2023a > /dev/null 2>&1
-
-# for reading the .pvtr files
-module load VTK > /dev/null 2>&1		
-
-# dependency of the Ceres least squares solver
-module load Eigen > /dev/null 2>&1    	
-module load Abseil > /dev/null 2>&1   				
-module load googletest > /dev/null 2>&1   	
+module load tools/prod GCCcore/14.3.0 CMake VTK	
 
 echo Finished loading modules.
 
