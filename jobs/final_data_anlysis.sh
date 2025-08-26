192903bd6e87ae9b9c0e8ddfcfcb1cd0ab032d19
avr24 <avr24@ic.ac.uk>
2025-08-26 16:36:19 +0100

    added a new cross validation program to properly do hyperparameter tuning without using the testing data and using cross validation because of the small dataset. This should actually provide valid results

--- jobs/final_data_anlysis.sh
+++ jobs/final_data_anlysis.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#PBS -l walltime=05:00:00
+#PBS -l walltime=12:00:00
 #PBS -l select=1:ncpus=16:mem=32gb:ompthreads=16
 #PBS -j oe
 #PBS -N final_data_analysis
 cd $PBS_O_WORKDIR
 
 cd ../python
-python final_data_analysis.py
+python cross_validation.py
